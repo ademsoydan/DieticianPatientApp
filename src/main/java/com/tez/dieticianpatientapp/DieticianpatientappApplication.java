bac223796bbcafd1a255c1769416c0ec8862bc4e
ademsoydan <86920390+AdemSoydan@users.noreply.github.com>
2024-03-16 19:13:15 +0300

    Initial commit

---
+++ src/main/java/com/tez/dieticianpatientapp/DieticianpatientappApplication.java
@@ -0,0 +1,13 @@
+package com.tez.dieticianpatientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieticianpatientappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DieticianpatientappApplication.class, args);
+	}
+
+}
