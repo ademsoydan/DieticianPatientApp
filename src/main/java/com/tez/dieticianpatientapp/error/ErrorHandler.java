497170a4e840aa5d03d90cd05af71999369e40fb
ademsoydan <ademm.soydan@gmail.com>
2024-04-12 20:40:25 +0300

    Add wrong password or tckn error message

--- src/main/java/com/tez/dieticianpatientapp/error/ErrorHandler.java
+++ src/main/java/com/tez/dieticianpatientapp/error/ErrorHandler.java
@@ -4,17 +4,20 @@ import com.tez.dieticianpatientapp.exception.NotUniqueTcknException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ErrorHandler {
-    @ExceptionHandler({NotUniqueTcknException.class, MethodArgumentNotValidException.class})
+    @ExceptionHandler({NotUniqueTcknException.class, MethodArgumentNotValidException.class, AuthenticationException.class})
     ResponseEntity<ApiError> handleException(Exception exception, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         ApiError apiError = new ApiError();
         apiError.setPath(request.getRequestURI());
         apiError.setMessage(exception.getMessage());
@@ -23,8 +26,11 @@ public class ErrorHandler {
             var validationErrors = ((MethodArgumentNotValidException)exception).getBindingResult().getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage, (existing, replacing) -> existing));
             apiError.setValidationErrors(validationErrors);
         }
-        apiError.setStatus(HttpStatus.BAD_REQUEST.value());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
-
+        else if(exception instanceof AuthenticationException){
+            apiError.setMessage("Şifre veya kullanıcı adı hatalı");
+            status = HttpStatus.UNAUTHORIZED;
+        }
+        apiError.setStatus(status.value());
+        return ResponseEntity.status(status).body(apiError);
     }
 }
