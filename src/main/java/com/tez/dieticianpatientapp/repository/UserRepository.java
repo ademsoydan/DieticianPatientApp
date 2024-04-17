eb1194f78cab157f43c10f975445e1270ac05c80
ademsoydan <ademm.soydan@gmail.com>
2024-04-17 18:36:53 +0300

    add Repository annotations for deployment

--- src/main/java/com/tez/dieticianpatientapp/repository/UserRepository.java
+++ src/main/java/com/tez/dieticianpatientapp/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.tez.dieticianpatientapp.repository;
 
 import com.tez.dieticianpatientapp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByTckn(String tckn);
 }
